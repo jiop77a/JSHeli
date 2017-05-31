6d4309741b9f990e89dc6054ec7c7dd9ee75d7e9
Alex E Scott <aescott@outlook.com>
2017-05-31 10:29:43 -0700

    make square bounce

--- assets/copter.js
+++ assets/copter.js
@@ -2,6 +2,9 @@ const canvasEl = document.getElementById("myCanvas");
 canvasEl.width = 1000;
 canvasEl.height = 500;
 const ctx = canvasEl.getContext("2d");
+var firstClick = true;
+let copterPos = 50;
+var falling;
 
 const drawBoard = () => {
 
@@ -13,16 +16,34 @@ const drawBoard = () => {
   Border.fillRect(0, 450, 1000, 50);
 };
 
-const drawCopter = (x, y) => {
+const drawCopter = (y = 20) => {
+  console.log(y);
   ctx.clearRect(0, 0, 1000, 500);
   drawBoard();
   ctx.fillStyle = "green";
-  ctx.fillRect(x, y, 20, 20);
+  ctx.fillRect(100, y, 20, 20);
   y += 1;
-  window.setTimeout(drawCopter, 20, x, y);
+  copterPos = y;
+  falling = window.setTimeout(drawCopter, 20, y);
 };
 
+const bounceCopter = () => {
+  ctx.clearRect(100, 100, 50, 500);
+  window.clearTimeout(falling);
+  copterPos -= 30;
+  drawCopter(copterPos);
+};
 
-document.addEventListener("DOMContentLoaded", drawBoard);
+document.addEventListener("DOMContentLoaded", () => {
+  drawBoard();
+});
 
-canvasEl.addEventListener("click", drawCopter(20,20));
+canvasEl.addEventListener("click", () => {
+  if (firstClick) {
+    drawCopter();
+    firstClick = false;
+
+  } else {
+    bounceCopter();
+  }
+});
