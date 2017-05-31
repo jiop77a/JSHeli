6179ecabab03ee0b43c8c8b600f77d9ae8035b37
Alex E Scott <aescott@outlook.com>
2017-05-30 22:26:34 -0700

    first movement of little square

--- assets/copter.js
+++ assets/copter.js
@@ -1,17 +1,28 @@
-document.addEventListener("DOMContentLoaded", function(){
-  const canvasEl = document.getElementById("myCanvas");
-  canvasEl.width = 500;
-  canvasEl.height = 500;
+const canvasEl = document.getElementById("myCanvas");
+canvasEl.width = 1000;
+canvasEl.height = 500;
+const ctx = canvasEl.getContext("2d");
 
-  const ctx = canvasEl.getContext("2d");
-  ctx.fillStyle = "purple";
-  ctx.fillRect(10, 10, 400, 500);
+const drawBoard = () => {
 
-  ctx.beginPath();
-  ctx.arc(100, 100, 20, Math.PI/2, Math.PI, true);
-  ctx.strokeStyle = "green";
-  ctx.lineWidth = 5;
-  ctx.stroke();
-  ctx.fillStyle = "blue";
-  ctx.fill();
-});
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, 1000, 500);
+  const Border = ctx;
+  Border.fillStyle = "blue";
+  Border.fillRect(0, 0, 1000, 50);
+  Border.fillRect(0, 450, 1000, 50);
+};
+
+const drawCopter = (x, y) => {
+  ctx.clearRect(0, 0, 1000, 500);
+  drawBoard();
+  ctx.fillStyle = "green";
+  ctx.fillRect(x, y, 20, 20);
+  y += 1;
+  window.setTimeout(drawCopter, 20, x, y);
+};
+
+
+document.addEventListener("DOMContentLoaded", drawBoard);
+
+canvasEl.addEventListener("click", drawCopter(20,20));
