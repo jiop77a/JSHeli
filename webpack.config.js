7adcb38f2d5f02142bd877829e848951d0951024
Alex E Scott <aescott@outlook.com>
2017-06-01 15:47:27 -0700

    rename entry file

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: "./assets/copter.js",
+  entry: "./assets/heli.js",
   output: {
       path: path.resolve(__dirname, 'lib'),
       filename: "bundle.js"
